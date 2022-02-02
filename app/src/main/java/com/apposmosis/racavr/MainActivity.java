c430aa343cc41c1b8623d94dd9ac8e5a4f7030ef
xdernin <xdernin@gmail.com>
2022-02-02 09:56:25 -0500

    Initial commit

---
+++ app/src/main/java/com/apposmosis/racavr/MainActivity.java
@@ -0,0 +1,21 @@
+package com.apposmosis.racavr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void cambiarlogin(View view){
+        Intent pantallalogin = new Intent(this,register.class);
+        startActivity(pantallalogin);
+
+    }
+}
